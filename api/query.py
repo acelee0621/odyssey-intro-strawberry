8ea0a646f512beb1a8b33a40d23270ed23540686
acelee0621 <acelee0621@outlook.com>
2025-03-17 11:27:59 +0800

    Update query.py

--- api/query.py
+++ api/query.py
@@ -1,18 +1,22 @@
 import strawberry
 
 from .types.playlist import Playlist
-
+from mock_spotify_rest_api_client.api.playlists import get_featured_playlists
 
 
 @strawberry.type
 class Query:
-        
     @strawberry.field(description="Playlists hand-picked to be featured to all users.")
-    def featured_playlists(self) -> list[Playlist]:
-        return [
-    Playlist(id="1", name="GraphQL Groovin'", description=None),
-    Playlist(id="2", name="Graph Explorer Jams", description=None),
-    Playlist(id="3", name="Interpretive GraphQL Dance", description=None),
-]
-    
-    
+    async def featured_playlists(self, info: strawberry.Info) -> list[Playlist]:
+        spotify_client = info.context["spotify_client"]
+        data = await get_featured_playlists.asyncio(client=spotify_client)
+        items = data.playlists.items
+        playlists = [
+            Playlist(
+                id=strawberry.ID(playlist.id),
+                name=playlist.name,
+                description=playlist.description,
+            )
+            for playlist in items
+        ]
+        return playlists
